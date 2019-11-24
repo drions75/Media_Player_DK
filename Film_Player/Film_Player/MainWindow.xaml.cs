c9e2db119997320ca564091b77697291ce3917a5
Drions <dkaniewski@lociechocinek.pl>
2019-11-24 16:47:05 +0100

    Dodano Slider, Style Slidera, Playback Slider

--- Film_Player/Film_Player/MainWindow.xaml.cs
+++ Film_Player/Film_Player/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace Film_Player
 {
@@ -20,14 +21,29 @@ namespace Film_Player
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        bool PlaybackSliderDragging = false;
         public MainWindow()
         {
             InitializeComponent();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 100);
+            timer.Tick += new EventHandler(Timer_Tick);
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if(!PlaybackSliderDragging)
+            {
+                PlaybackSlider.Value = Me.Position.TotalMilliseconds;
+            }
         }
 
         private void StartBtn_Click(object sender, RoutedEventArgs e)
         {
+            //Me.SpeedRatio = SpeedSlider.Value;
+            Me.Volume = VolumeSlider.Value;
+            timer.Start();
             Me.Play();
+            
         }
 
         private void StopBtn_Click(object sender, RoutedEventArgs e)
@@ -44,5 +60,33 @@ namespace Film_Player
         {
             Me.Volume = VolumeSlider.Value;
         }
+
+        private void PlaybackSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if(PlaybackSliderDragging)
+            {
+                Me.Position = TimeSpan.FromMilliseconds(PlaybackSlider.Value);
+            }
+        }
+
+        private void Me_MediaOpened(object sender, RoutedEventArgs e)
+        {
+            PlaybackSlider.Maximum = Me.NaturalDuration.TimeSpan.TotalMilliseconds;
+            //SpeedSlider.Value =1;
+        }
+
+        private void PlaybackSlider_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            PlaybackSliderDragging = false;
+            Me.Play();
+        }
+
+        private void PlaybackSlider_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            PlaybackSliderDragging = true;
+            Me.Stop();
+        }
+
+       
     }
 }
